16d642d76bdc3336f7eb7d0a6e5a1e55bc0e9a1c
ljh <lijinhong361@163.com>
2019-06-12 00:46:07 +0800

    往商品数据库补全数据

--- goods/models.py
+++ goods/models.py
@@ -13,15 +13,15 @@ class GoodTypeInfo(models.Model):
 class GoodInfo(models.Model):
     #商品的名称
     gtitle = models.CharField(max_length=32)
-    gpic = models.ImageField(upload_to='goods') # upload_to是什么？
+    gpic = models.ImageField(upload_to='image/goods') # upload_to是什么？
     # 商品价格
     gprice = models.DecimalField(max_digits=6,decimal_places=2)
     #商品的单位
     guntil = models.CharField(max_length=10,default='500g')
     isDelete = models.BooleanField(default=0)
-    gclick = models.IntegerField()
+    gclick = models.IntegerField(default=0)
     gintroduction = models.CharField(max_length= 250)
-    stock = models.IntegerField()
+    stock = models.IntegerField(default=0)
     gcontent = models.TextField(blank=True)
     gadv = models.BooleanField(default=0)
     gtype = models.ForeignKey('GoodTypeInfo',on_delete=models.CASCADE)
--- user_manage/admin.py
+++ user_manage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import UserInfo,UserAdress
 # Register your models here.
+
+admin.site.register(UserInfo)
+admin.site.register(UserAdress)
